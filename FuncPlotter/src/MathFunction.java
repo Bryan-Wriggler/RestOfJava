296b3d16b6d860a4fea19cf3f4aa1899fd7d5c41
s-zihhsieh <s-zihhsieh@484E293886.lwsd.k12wa.org>
2022-05-17 14:55:02 -0700

    change functional interface to classes

--- FuncPlotter/src/MathFunction.java
+++ FuncPlotter/src/MathFunction.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
-public interface MathFunction {
+public interface MathFunction { 
 	public int function(int x);
+	
+	public Color getColor();
 }
+
+/*
+ * if it has only 1 method, it is functional interface
+ * -> MathFunction name = (parameter) -> { return returnType;};
+ * 
+ * if it has 2 methods, it is a class
+ * -> need new
+ * -> MathFunction sin = new MathFunction() {
+ * 		public int function(int x) { return returnType1;}
+ * 
+ * 		public Color getColor() { return returnType2;}
+ * 	  }
+ */
--- FuncPlotter/src/Program.java
+++ FuncPlotter/src/Program.java
@@ -11,7 +11,7 @@ public class Program {
         // Create the canvas instance, set its range to x:[-360, 360] and y:[-240, 240] then 
         // open it on the screen. Use canvas.plot(pX, pY) to plot a point at the (pX, pY) coords
         canvas = new Canvas();
-        canvas.setRange(-360, -240, 360, 240);
+        canvas.setRange(-360, -360, 360, 360);
         canvas.open();
 
         // Draw a short red diagonal on the canvas
@@ -19,28 +19,133 @@ public class Program {
         canvas.setColor(Color.red);
         
         //sin function
-        MathFunction sin = (x) -> { 
-        	return (int) (200 * Math.sin(Math.PI * x / 360));
+        MathFunction sin = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) (240 * Math.sin(Math.PI * x / 360));
+        	}
+        	
+        	public Color getColor() {
+        		return Color.blue;
+        	}
         };
         
         //circle function
-        MathFunction circle = (x) -> { 
-        	return (int) (Math.sqrt(240 * 240 - x * x));
+        MathFunction circle = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) (Math.sqrt(120 * 120 - x * x));
+        	}
+        	
+        	public Color getColor() {
+        		return Color.MAGENTA;
+        	}
         };
         
         //absolute value function
-        MathFunction absolute = (x) -> {
-        	return Math.abs(x);
+        MathFunction absolute = new MathFunction() { 
+        	public int function(int x) {
+        		return Math.abs(x);
+        	}
+        	
+        	public Color getColor() {
+        		return Color.green;
+        	}
         };
         
-        for (int x = -240; x <= 240; x++) {
-        	int y = circle.function(x);
-            canvas.plot(x, y);
-            canvas.plot(x,  -y);
-        }
+        //using arcsin and cos to make circle
+        MathFunction circle2 = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) (240 * Math.cos(Math.asin((double) x/240)));
+        	}
+        	
+        	public Color getColor() {
+        		return Color.red;
+        	}
+        };
+        
+        //quadratic equation
+        MathFunction quadratic = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) (((double) x * x - 200 * 200) / 250);
+        	}
+        	
+        	public Color getColor() {
+        		return Color.blue;
+        	}
+        };
+        
+        //logarithm with base 2
+        MathFunction log2 = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) (20 * Math.log(Math.abs(x)));
+        	}
+        	
+        	public Color getColor() {
+        		return Color.blue;
+        	}
+        };
+        
+        //cubic equation
+        MathFunction cubic = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) (Math.pow(x, 3) / 2000);
+        	}
+        	
+        	public Color getColor() {
+        		return Color.blue;
+        	}
+        };
+        
+        //hyperbola with y-intersect
+        MathFunction hyperbolaV = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) Math.sqrt(40 * 40 + x * x);
+        	}
+        	
+        	public Color getColor() {
+        		return Color.cyan;
+        	}
+        };
+        
+        //hyperbola with x-intersect
+        MathFunction hyperbolaH = new MathFunction() { 
+        	public int function(int x) {
+        		return (int) Math.sqrt(x * x - 40 * 40);
+        	}
+        	
+        	public Color getColor() {
+        		return Color.cyan;
+        	}
+        };
+        
+        //arrays of functions
+        MathFunction[] functions = {sin, quadratic, log2, cubic};
+        
+        plotFunction(circle, circle2,hyperbolaV, hyperbolaH);
+        //plotFunction(functions);
+        
+        //plotFunction(functions, sin); -> this is invalid
 
         // Pause and close the canvas then terminate the program.
         canvas.pause();
         canvas.close();
+    }
+    
+    /**
+     * @param function -> the function that plugged into the graph
+     * 
+     * plots a given function
+     */
+    public static void plotFunction(MathFunction... function) { 
+    	//if using ..., it is either throwing multiple object with same types
+    	//... can also throw arrays into it, but never throwing arrays "and" object
+    	//another characteristic: can throw nothing as parameter (which is an array of 0 length)
+    	
+    	for (int i=0; i < function.length; i++) {
+    		for (int x = -360; x <= 360; x++) {
+    			canvas.setColor(function[i].getColor());
+        		canvas.plot(x,  function[i].function(x));
+        		canvas.plot(x,  -function[i].function(x));
+        	}
+    	}
     }
 }
