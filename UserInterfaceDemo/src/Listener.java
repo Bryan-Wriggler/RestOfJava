23807265d4dc043a809d43b3f9b570a16e2f051d
s-zihhsieh <s-zihhsieh@484E293886.lwsd.k12wa.org>
2022-06-09 12:21:05 -0700

    Create new actions and components

--- UserInterfaceDemo/src/Listener.java
+++ UserInterfaceDemo/src/Listener.java
@@ -1,9 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.*;
 
-public class Listener implements WindowListener {
+public class Listener implements WindowListener, ActionListener {
+	//field
+	private TextArea textArea;
+	
+	//a constructor to get all components needed in this class 
+	public Listener(TextArea textArea) {
+		this.textArea = textArea;
+	}
 
+	//window listener
 	@Override
 	public void windowActivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
@@ -52,4 +62,24 @@ public class Listener implements WindowListener {
 		
 	}
 
+	
+	//action listener
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		Button button = (Button) arg0.getSource(); //source of this event
+		
+		//if we want to print in text area, we need to get text area
+		//we put text area as a component needed by constructor, so we can access it
+		printToTextArea(button.getLabel());
+	}
+
+	
+	//other methods
+	private void printToTextArea(String text) {
+		String current = textArea.getText(); //get the current text
+		String newText = current + "\n" + text;
+		textArea.setText(newText);
+		textArea.setCaretPosition(newText.length());
+	}
 }
--- UserInterfaceDemo/src/Program.java
+++ UserInterfaceDemo/src/Program.java
@@ -1,21 +1,64 @@
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Insets;
+import java.awt.TextArea;
 
 public class Program {
+	
+	//constants
+	private static final int _padding = 10;
+	private static Button button = new Button("This is Button1");
+	private static TextArea myTextArea = new TextArea();
+	private static Listener myListener = new Listener(myTextArea); //need to be at the last, so the code will work
+	
+	//fields
+	public static Frame myFrame = new Frame("Java UI Demo");
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Java UI Demo");
 		
 		//start by creating frame (basic area for UI)
-		Frame myFrame = new Frame("Java UI Demo");
 		myFrame.setSize(971, 600); //size
 		myFrame.setVisible(true); //let it can be see by us
 		
 		//add listener
-		Listener myListener = new Listener();
-		myFrame.addWindowListener(myListener);
+		myFrame.addWindowListener(myListener); //add window listener interface
+		myFrame.setLayout(null);
+		
+		Dimension d = myFrame.getSize();
+		System.out.println(d);
+		
+		Insets i = myFrame.getInsets();
+		System.out.println(i);
+		
+		//create a new button
+		button.setBounds(i.left + _padding, i.top + _padding, 100, 40); //(x, y, length, width)
+		myFrame.add(button); //add it to the frame
+		button.setBackground(Color.lightGray);
+		//listen event
+		button.addActionListener(myListener); //add action listener interface
+		
+		//create test area
+		int xAnchor = button.getX(); //top left coordinate of button
+		int yAnchor = button.getY() + button.getHeight() + _padding; //top coordinate of button + its hight + padding
+		myTextArea.setBounds(xAnchor, yAnchor, 
+				myFrame.getWidth() - i.left - i.right - 2 * _padding, //width wanted for text area
+				myFrame.getHeight() - yAnchor - i.bottom - _padding);
+		myFrame.add(myTextArea);
+		myTextArea.setBackground(Color.pink);
+		myTextArea.setEditable(false); //meaning read only
 		
 		System.out.println("Goodbye");
 		
 		//close event
 	}
 }
+
+/*
+ * The (0, 0) will be at the top left extreme "of the entire window" (not only the white space)
+ * 
+ * though the visible space are just positive (and limited by dimension), but it can appear in
+ * negative space (not visible, but exist), even outside the dimension (not visible, but also exist)
+ */
